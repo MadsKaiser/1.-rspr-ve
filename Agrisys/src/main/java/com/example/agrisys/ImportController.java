de7a12b28ecf00e985892777261b917cd65b550d
Mads Ringgaard Kaiser <madka01@eamv.dk>
2025-05-01 11:23:00 +0200

    Add files via upload

---
+++ Agrisys/src/main/java/com/example/agrisys/ImportController.java
@@ -0,0 +1,11 @@
+package com.example.agrisys;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ImportController {
+
+    @FXML
+    private Button BrowseID;
+
+}
---
+++ Agrisys/src/main/java/com/example/agrisys/MenuController.java
@@ -0,0 +1,4 @@
+package com.example.agrisys;
+
+public class MenuController {
+}
