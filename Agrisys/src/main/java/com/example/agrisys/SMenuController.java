121f3de497f7a34749b5f9ab7e8515ac29f72bb1
MadsKaiser <madka01@eamv.dk>
2025-05-12 12:40:08 +0200

    Widget plus SQL

--- Agrisys/src/main/java/com/example/agrisys/SMenuController.java
+++ Agrisys/src/main/java/com/example/agrisys/SMenuController.java
@@ -1,17 +1,25 @@
 package com.example.agrisys;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.ScatterChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class SMenuController {
     @FXML
@@ -33,15 +41,15 @@ public class SMenuController {
     @FXML
     private CheckBox Widget1;
     @FXML
-    private CheckBox Widget2; // Declared Widget2
+    private CheckBox Widget2;
     @FXML
-    private CheckBox Widget3; // Declared Widget3
+    private CheckBox Widget3;
     @FXML
-    private CheckBox Widget4; // Declared Widget4
+    private CheckBox Widget4;
     @FXML
-    private CheckBox Widget5; // Declared Widget5
+    private CheckBox Widget5;
     @FXML
-    private CheckBox Widget6; // Declared Widget6
+    private CheckBox Widget6;
     @FXML
     private Label Label1;
     @FXML
@@ -55,7 +63,7 @@ public class SMenuController {
     @FXML
     private Label Label6;
     @FXML
-    private AnchorPane Anchor; // AnchorPane for graph placement
+    private AnchorPane Anchor;
 
     @FXML
     private void toggleMenuVisibility() {
@@ -63,7 +71,6 @@ public class SMenuController {
     }
 
     public void initialize() {
-        // Ensure the hidden menu and labels are initially invisible
         hiddenMenu.setVisible(false);
         Label1.setVisible(false);
         Label2.setVisible(false);
@@ -72,7 +79,6 @@ public class SMenuController {
         Label5.setVisible(false);
         Label6.setVisible(false);
 
-        // Set button actions
         AlarmButton.setOnAction(e -> loadScene("Alarm.fxml", AlarmButton));
         WidgetsButton.setOnAction(e -> toggleMenuVisibility());
         LogoutButton.setOnAction(e -> loadScene("Login.fxml", LogoutButton));
@@ -80,21 +86,51 @@ public class SMenuController {
         ImportCSVButton.setOnAction(e -> loadScene("ImportCSV.fxml", ImportCSVButton));
         DashboardsButton.setOnAction(e -> loadScene("Dashboard.fxml", DashboardsButton));
 
-        // Set actions for widgets to toggle corresponding label visibility
-        Widget1.setOnAction(event -> Label1.setVisible(Widget1.isSelected()));//skriv kode der laver widgets
+        Widget1.setOnAction(event -> {
+            Label1.setVisible(Widget1.isSelected());
+            if (Widget1.isSelected()) {
+                addGraphToPane();
+            }
+        });
         Widget2.setOnAction(event -> Label2.setVisible(Widget2.isSelected()));
         Widget3.setOnAction(event -> Label3.setVisible(Widget3.isSelected()));
         Widget4.setOnAction(event -> Label4.setVisible(Widget4.isSelected()));
         Widget5.setOnAction(event -> Label5.setVisible(Widget5.isSelected()));
         Widget6.setOnAction(event -> Label6.setVisible(Widget6.isSelected()));
-
-        Widget1.setOnAction(e -> {
-            if (Widget1.isSelected()) {
-                GraphPlaceholder.addGraphToPane(Anchor);
-            }
-        });
     }
 
+    private void addGraphToPane() {
+        NumberAxis xAxis = new NumberAxis();
+        xAxis.setLabel("Responder");
+
+        NumberAxis yAxis = new NumberAxis();
+        yAxis.setLabel("FCR");
+
+        ScatterChart<Number, Number> scatterChart = new ScatterChart<>(xAxis, yAxis);
+        scatterChart.setTitle("Responder vs FCR");
+
+        try (Connection connection = DatabaseManager.getConnection()) {
+            String query = "SELECT Responder, Duration FROM dbo.[Visit data]";
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+
+            XYChart.Series<Number, Number> series = new XYChart.Series<>();
+            series.setName("PPT Data");
+
+            while (resultSet.next()) {
+                double responder = resultSet.getDouble("Responder");
+                double fcr = resultSet.getDouble("FCR");
+                series.getData().add(new XYChart.Data<>(responder, fcr));
+            }
+
+            scatterChart.getData().add(series);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Anchor.getChildren().clear();
+        Anchor.getChildren().add(scatterChart);
+    }
 
     private void loadScene(String fxmlFile, Button button) {
         try {
