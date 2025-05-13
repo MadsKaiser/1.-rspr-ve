e9b8ba441f362f524e0c037bc4a7c850912bccea
MadsKaiser <madka01@eamv.dk>
2025-05-13 10:39:15 +0200

    Widget plus SQL

--- Agrisys/src/main/java/com/example/agrisys/SMenuController.java
+++ Agrisys/src/main/java/com/example/agrisys/SMenuController.java
@@ -1,10 +1,6 @@
 package com.example.agrisys;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.ScatterChart;
 import javafx.scene.chart.XYChart;
@@ -12,8 +8,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -89,38 +88,82 @@ public class SMenuController {
         Widget1.setOnAction(event -> {
             Label1.setVisible(Widget1.isSelected());
             if (Widget1.isSelected()) {
-                addGraphToPane();
+                addLineChartToPane();
             }
         });
-        Widget2.setOnAction(event -> Label2.setVisible(Widget2.isSelected()));
+        Widget2.setOnAction(event -> {
+            Label2.setVisible(Widget2.isSelected());
+            if (Widget2.isSelected()) {
+                addScatterChartToPane();
+            }
+        });
         Widget3.setOnAction(event -> Label3.setVisible(Widget3.isSelected()));
         Widget4.setOnAction(event -> Label4.setVisible(Widget4.isSelected()));
         Widget5.setOnAction(event -> Label5.setVisible(Widget5.isSelected()));
         Widget6.setOnAction(event -> Label6.setVisible(Widget6.isSelected()));
     }
 
-    private void addGraphToPane() {
+    private void addLineChartToPane() {
         NumberAxis xAxis = new NumberAxis();
-        xAxis.setLabel("Responder");
+        xAxis.setLabel("Responder Index");
+
+        NumberAxis yAxis = new NumberAxis();
+        yAxis.setLabel("FCR");
+
+        LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
+        lineChart.setTitle("Responder Index vs FCR");
+
+        try (Connection connection = DatabaseManager.getConnection();
+             PreparedStatement statement = connection.prepareStatement(
+                     "SELECT Responder, FCR FROM madserkaiser_dk_db_agrisys.dbo.[PPT data]");
+             ResultSet resultSet = statement.executeQuery()) {
+
+            XYChart.Series<Number, Number> series = new XYChart.Series<>();
+            series.setName("Foderudnyttelse");
+
+            int index = 1; // Start index for responders
+            while (resultSet.next()) {
+                double fcr = resultSet.getDouble("FCR");
+                if (fcr < -500 || fcr > 1000) {
+                    continue; // Skip invalid FCR values
+                }
+                series.getData().add(new XYChart.Data<>(index++, fcr));
+            }
+
+            lineChart.getData().add(series);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Anchor.getChildren().clear();
+        Anchor.getChildren().add(lineChart);
+    }
+
+    private void addScatterChartToPane() {
+        NumberAxis xAxis = new NumberAxis();
+        xAxis.setLabel("Daglig tilvækst");
 
         NumberAxis yAxis = new NumberAxis();
         yAxis.setLabel("FCR");
 
         ScatterChart<Number, Number> scatterChart = new ScatterChart<>(xAxis, yAxis);
-        scatterChart.setTitle("Responder vs FCR");
+        scatterChart.setTitle("FCR vs. tilvækst");
 
-        try (Connection connection = DatabaseManager.getConnection()) {
-            String query = "SELECT Responder, FCR FROM madserkaiser_dk_db_agrisys.dbo.[PPT data]";
-            PreparedStatement statement = connection.prepareStatement(query);
-            ResultSet resultSet = statement.executeQuery();
+        try (Connection connection = DatabaseManager.getConnection();
+             PreparedStatement statement = connection.prepareStatement(
+                     "SELECT [Weight gain kg], FCR FROM madserkaiser_dk_db_agrisys.dbo.[PPT data]");
+             ResultSet resultSet = statement.executeQuery()) {
 
             XYChart.Series<Number, Number> series = new XYChart.Series<>();
-            series.setName("PPT data");
+            series.setName("Foderudnyttelse vs Tilvækst");
 
             while (resultSet.next()) {
-                double responder = resultSet.getDouble("Responder");
+                double weightGain = resultSet.getDouble("Weight gain kg");
                 double fcr = resultSet.getDouble("FCR");
-                series.getData().add(new XYChart.Data<>(responder, fcr));
+                if (fcr < -500 || fcr > 1000 || weightGain < 0) {
+                    continue; // Skip invalid values
+                }
+                series.getData().add(new XYChart.Data<>(weightGain, fcr));
             }
 
             scatterChart.getData().add(series);
