0c72b2d7ac67815658488da719ec1ad4b19715d2
iiiiggg-hub <iggythefrogster@gmail.com>
2025-05-15 10:57:08 +0200

    Fixet Morten's Knap

--- Agrisys/src/main/java/com/example/agrisys/DashboardController.java
+++ Agrisys/src/main/java/com/example/agrisys/DashboardController.java
@@ -2,22 +2,21 @@ package com.example.agrisys;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class DashboardController {
+
     @FXML
     private Button BackToMenuButton;
+
     @FXML
     private CheckBox PresetDashboard1;
+
+    @FXML
+    public void initialize() {
+        BackToMenuButton.setOnAction(e -> HelperMethods.loadScene("SMenu.fxml", BackToMenuButton));
+    }
 
     @FXML
     private void handleDashboardCheckBox() {
@@ -29,10 +28,4 @@ public class DashboardController {
             // Add logic for deactivating widgets here
         }
     }
-    // Button eksistere ikke på dashboard???
-    @FXML
-    private void initilize() {
-        BackToMenuButton.setOnAction(e -> HelperMethods.loadScene("SMenu.fxml", BackToMenuButton));
-    }
 }
-
