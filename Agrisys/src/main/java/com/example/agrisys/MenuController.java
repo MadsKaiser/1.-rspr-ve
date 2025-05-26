d6b17be7c13b6c5dc67642173a8d7557b82116a0
iiiiggg-hub <iggythefrogster@gmail.com>
2025-05-26 20:46:25 +0200

    fikset 2

--- Agrisys/src/main/java/com/example/agrisys/MenuController.java
+++ Agrisys/src/main/java/com/example/agrisys/MenuController.java
@@ -5,6 +5,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
+import java.util.Map;
+
 import static com.example.agrisys.HelperMethods.loadScene;
 
 public class MenuController {
@@ -18,28 +20,16 @@ public class MenuController {
     private Button ExportCSVButton;
 
     public void initialize() {
-        displaySelectedKPIs();
+        loadKPIs(KPIStorage.getSavedKPIsWithValues());
         LogoutButton.setOnAction(e -> loadScene("Login.fxml", LogoutButton));
         AlarmButton.setOnAction(e -> loadScene("Alarm.fxml", AlarmButton));
         ExportCSVButton.setOnAction(e -> loadScene("Export.fxml", ExportCSVButton));
     }
-
-    private void displaySelectedKPIs() {
-        double yPosition = 10.0; // Initial Y position for displaying KPIs
-        for (String kpi : KPIStorage.getSavedKPIs()) {
-            try {
-                // Create and configure the KPI label
-                Label kpiLabel = new Label(kpi);
-                kpiLabel.setLayoutX(50.0); // X position
-                kpiLabel.setLayoutY(yPosition); // Y position
-                kpiLabel.setStyle("-fx-font-size: 14px; -fx-font-weight: bold;");
-
-                // Add the label to the AnchorPane
-                AnchorPane.getChildren().add(kpiLabel);
-            } catch (Exception e) {
-                System.err.println("Failed to load KPI: " + e.getMessage());
-            }
-            yPosition += 40.0; // Increment Y position for the next KPI
-        }
+    public void loadKPIs(Map<String, String> kpiValues) {
+        kpiValues.forEach((kpi, value) -> {
+            Label kpiLabel = new Label(kpi + ": " + value);
+            kpiLabel.setStyle("-fx-font-size: 14px; -fx-font-weight: bold;");
+            AnchorPane.getChildren().add(kpiLabel);
+        });
     }
 }
