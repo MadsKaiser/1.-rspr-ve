6f28df0a0c36ece175a8aca1751f9f06d497c1e0
MadsKaiser <madka01@eamv.dk>
2025-05-28 12:27:31 +0200

    Det hele kan ses i Bruger

---
+++ Agrisys/src/main/java/com/example/agrisys/GraphStorage.java
@@ -0,0 +1,29 @@
+package com.example.agrisys;
+
+import javafx.scene.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GraphStorage {
+    private static final GraphStorage instance = new GraphStorage();
+    private final List<Node> graphs = new ArrayList<>();
+
+    private GraphStorage() {}
+
+    public static GraphStorage getInstance() {
+        return instance;
+    }
+
+    public void addGraph(Node graph) {
+        graphs.add(graph);
+    }
+
+    public List<Node> getGraphs() {
+        return new ArrayList<>(graphs);
+    }
+
+    public void clearGraphs() {
+        graphs.clear();
+    }
+}
